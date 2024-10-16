63ed25bdffb520c26ddd7aa462a1c83f76697682
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-15 19:51:24 -0500

    first commit

---
+++ src/main/java/com/usersserviceapi/UsersServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.usersserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usersserviceapi/UsersServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usersserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
