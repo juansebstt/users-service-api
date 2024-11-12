3e75a09533e2a6380f21a34b68504aa81160cffa
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-12 17:33:00 -0500

    Implemented ApiPathConstants

--- src/main/java/com/usersserviceapi/common/constant/ApiPathConstants.java
+++ src/main/java/com/usersserviceapi/common/constant/ApiPathConstants.java
@@ -1,4 +1,8 @@
 package com.usersserviceapi.common.constant;
 
 public class ApiPathConstants {
+
+    public static final String V1_ROUTE = "/v1";
+    public static final String USER_ROUTE = "/user";
+
 }
