a4508c3abe071e842fec5c386dd6d7e5db8b35c7
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-12 16:54:19 -0500

    Implemented api spring context

--- src/main/java/com/usersserviceapi/controller/impl/UserController.java
+++ src/main/java/com/usersserviceapi/controller/impl/UserController.java
@@ -1,4 +1,7 @@
 package com.usersserviceapi.controller.impl;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 }
--- src/main/java/com/usersserviceapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/usersserviceapi/service/impl/UserServiceImpl.java
@@ -1,4 +1,7 @@
 package com.usersserviceapi.service.impl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl {
 }
