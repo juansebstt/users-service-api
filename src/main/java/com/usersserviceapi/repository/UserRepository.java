45d70ad2e33a2995365efda6ea8de85449ac59b8
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-13 18:22:34 -0500

    Implemented UserRepository

--- src/main/java/com/usersserviceapi/repository/UserRepository.java
+++ src/main/java/com/usersserviceapi/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package com.usersserviceapi.repository;
 
+import com.usersserviceapi.common.entity.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+    Optional<UserModel> findByEmail(String email);
+
 }
